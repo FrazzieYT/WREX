c05efc2c2304712695f5c09a72ca9a1594a00e57
FrazzieYT <kenfrazzi@gmail.com>
2026-06-15 23:47:11 +0300

    Добавил ссылку

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Input;
 using SystemManager.ViewModels;
 
 namespace SystemManager
@@ -9,6 +12,26 @@ namespace SystemManager
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+        }
+        private void GitHubLink_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "https://github.com/FrazzieYT/WREX",
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Не удалось открыть ссылку в браузере.\n\nОшибка: " + ex.Message,
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
         }
     }
 }
